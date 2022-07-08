c19701dcc8ab9ac214e8d74b87fb9f8752bf4482
Lucas Matheus <lucasmgsilva@hotmail.com>
2022-07-08 20:43:45 -0300

    Estrutura inicial + Área de nova tarefa

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Header } from './components/Header'
+import { NewTask } from './components/NewTask'
+
+import styles from './App.module.css'
+
+import './global.css'
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <div className={styles.wrapper}>
+        <NewTask />
+        <main>
+
+        </main>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import ToDoListLogo from '../assets/todo-list-logo.svg'
+
+import styles from './Header.module.css'
+
+export function Header () {
+    return (
+        <header className={styles.header}>
+            <img src={ToDoListLogo}/>
+        </header>
+    )
+}
